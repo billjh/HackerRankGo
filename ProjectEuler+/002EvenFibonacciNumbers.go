fd9cde16621979ede5ff5726597b8223b938ec8e
Huan Jiang <bill.jiangh@gmail.com>
2016-12-04 12:16:55 +0800

    project euler+ 002

---
+++ ProjectEuler+/002EvenFibonacciNumbers.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+    var n int
+    fmt.Scan(&n)
+    for i := 0; i < n; i++ {
+        var a int
+        fmt.Scan(&a)
+        fmt.Println(sum(a))
+    }
+}
+
+func sum(limit int) int {
+    a, b, sum := 1, 2, 2
+    for {
+        a, b = b, a+b
+        if b >= limit {
+            break
+        }
+        if b%2 == 0 {
+            sum += b
+        }
+    }
+    return sum
+}
