e474d18c3be6d86b2be2de1ac99f79900099e20d
Huan Jiang <bill.jiangh@gmail.com>
2016-12-06 14:57:10 +0800

    project euler+ 009 special pythagorean triplet

---
+++ ProjectEuler+/009SpecialPythagoreanTriplet.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+    var t int
+    fmt.Scan(&t)
+    for i := 0; i < t; i++ {
+        var n int
+        fmt.Scan(&n)
+        fmt.Println(findLargest(n))
+    }
+}
+
+func findLargest(n int) int {
+    max := -1
+    // a >= b >= c
+    for a := n / 3; a < n/2; a++ {
+        for c := 1; c < (n-a)/2+1; c++ {
+            b := n - a - c
+            if a*a == b*b+c*c && a*b*c > max {
+                max = a * b * c
+            }
+        }
+    }
+    return max
+}
