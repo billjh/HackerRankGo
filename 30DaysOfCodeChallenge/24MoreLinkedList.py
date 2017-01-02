a738ae983453ebee811af113a360e61aba729bdb
Huan Jiang <bill.jiangh@gmail.com>
2017-01-02 22:13:13 +0800

    24 day: more linked list

---
+++ 30DaysOfCodeChallenge/24MoreLinkedList.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+class Solution:
+    def insert(self,head,data):
+            p = Node(data)
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+    def removeDuplicates(self,head):
+        #Write your code here
+        if head == None:
+            return head
+        # Cache the current Node and data
+        cur, value = head, head.data
+        while cur.next != None:
+            if value != cur.next.data:
+                # Refresh cache
+                cur, value = cur.next, cur.next.data
+            else:
+                # Skip the Node by redirecting next pointer
+                cur.next = cur.next.next
+        return head
+
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)
+head=mylist.removeDuplicates(head)
+mylist.display(head);
